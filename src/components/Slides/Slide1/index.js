4711bcd027189acf29dbb676e4462e0d0e5eea95
ValentinoT <danieltorresc13@gmail.com>
2020-07-20 16:44:52 -0500

    Inicio Slider

--- src/components/Slides/Slide1/index.js
+++ src/components/Slides/Slide1/index.js
@@ -1,11 +1,28 @@
 import React from 'react'
-import { Container } from './styles'
+import { Container, Content } from './styles'
 import Image from '../../image'
+import { Grid, Typography } from '@material-ui/core'
+import { LinkButton } from '../../generals/Link'
+import { NavigateNext } from '@material-ui/icons'
 
 export const Slide1 = () => {
   return (
-    <Container>
-      <Image name='1.jpg' />
+    <Container container>
+      <Content item md={5} xl={5}>
+        <Typography variant='h3'>Arquitectura y</Typography>
+        <Typography variant='h3'>Construcción</Typography>
+        <Typography variant='body1'>La arquitectura comercial se ha convertido en una sofisticada estrategia de marketing.</Typography>
+        <LinkButton
+          endIcon={<NavigateNext />}
+          variant='outlined'
+        >
+            Arquitectura
+        </LinkButton>
+      </Content>
+      <Grid item md={7} xl={7}>
+        <Image name='imageBanner1.png' />
+      </Grid>
+
     </Container>
   )
 }
--- src/components/Slides/Slide1/styles.js
+++ src/components/Slides/Slide1/styles.js
@@ -1,6 +1,7 @@
 import { styled } from '@material-ui/styles'
+import { Grid } from '@material-ui/core'
 
-export const Container = styled('div')({
+export const Container = styled(Grid)({
   background: 'transparent',
   width: '100%',
   height: '100vh',
@@ -8,5 +9,16 @@ export const Container = styled('div')({
   '& .gatsby-image-wrapper': {
     width: '100%',
     height: '100%'
+  },
+  '& .gatsby-image-wrapper img': {
+    objectFit: 'contain!important',
+    objectPosition: 'bottom!important'
   }
+})
+
+export const Content = styled(Grid)({
+  padding: '80px 40px',
+  display: 'flex',
+  flexDirection: 'column',
+  justifyContent: 'center'
 })
