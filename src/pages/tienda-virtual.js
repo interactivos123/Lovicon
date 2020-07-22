dd3e68bb10f602fadc58c321744ed14eaa47a170
Simon Torres <simontorres979797@gmail.com>
2020-07-22 10:09:43 -0500

    agregando pagina de tienda virtual

--- src/pages/tienda-virtual.js
+++ src/pages/tienda-virtual.js
@@ -1,10 +1,13 @@
 import React from 'react'
+import { Cabecera } from '../components/generals/Cabecera'
+import { Vacio } from '../components/generals/Vacio'
 
 export const Tiendavirtual = () => {
   return (
-    <div>
-      Tienda Virtual
-    </div>
+    <>
+      <Cabecera title='Tienda Virtual' />
+      <Vacio />
+    </>
   )
 }
 
